026666c49ca5963a2c187ae821cd648f2ae1f011
BenjaminMiinch <47015766+BenjaminMiinch@users.noreply.github.com>
2019-01-25 22:36:25 -0600

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
+int main(void)
+{
 int i;
 
 for ( i = 1; i <= 100; ++i)
 
 {	if( i%3 == 0)
-	{printf("Fizz");
+	{printf("Fizz")};
 	if( i%5 == 0)
-	{prinf("Fuzz");}
+	{printf("Fuzz")};
 	if( i%3 == 0 && i%5 == 0)
 	{printf("Fizz-Fuzz!")};
 }
